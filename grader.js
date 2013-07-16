c3f64ae44acb8e9f627697e86b30db33b4922684
msyaono <msyaono@rockos.co.jp>
2013-07-16 00:01:48 +0000

    hw4 first responsive page

--- grader.js
+++ grader.js
@@ -24,9 +24,10 @@
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var restler = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
-
+var URL_DEFAULT = "http://google.com";
 var assertFileExists = function(infile) {
     var instr = infile.toString();
     if(!fs.existsSync(instr)) {
@@ -35,9 +36,30 @@ var assertFileExists = function(infile) {
     }
     return instr;
 };
+var assertUrlExists = function(inUrl) {
+    console.log('inurl ' + inUrl);
+    if (inUrl ==='') {
+        console.log("%s does not exist. Exiting.", instr);
+        process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
+    }
+    /*
+    restler.get(inUrl)
+        .on('complete', function(data) {
+            return inUrl.toString();
+    })
+    .on('error', function(err) {
+        console.log("%s does not exist. Exiting.", inUrl);
+        process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
+    });
+    */
+    return inUrl;
+};
 
 var cheerioHtmlFile = function(htmlfile) {
     return cheerio.load(fs.readFileSync(htmlfile));
+};
+var cheerioUrlFile = function(urlfile) {
+    return cheerio.load(urlfile);
 };
 
 var loadChecks = function(checksfile) {
@@ -54,15 +76,42 @@ var checkHtmlFile = function(htmlfile, checksfile) {
     }
     return out;
 };
+var checkHtmlUrl = function(urlfile, checksfile) {
+    restler.get(urlfile)
+        .on('complete', function(data) {
+            $ = cheerioUrlFile(data);
+            var checks = loadChecks(checksfile).sort();
+            var out = {};
+
+            for(var ii in checks) {
+                var present = $(checks[ii]).length > 0;
+                out[checks[ii]] = present;
+                //console.log("checks " + checks[ii] + ', out '+present);
+            }
+            console.log(JSON.stringify(out, null, 4));
+    //        return out;
+        })
+    .on('error', function(err) {
+        console.log('error restler.');
+    });
+};
 
 if(require.main == module) {
     program
         .option('-c, --checks ', 'Path to checks.json', assertFileExists, CHECKSFILE_DEFAULT)
         .option('-f, --file ', 'Path to index.html', assertFileExists, HTMLFILE_DEFAULT)
+        .option('-u,--url <url>', 'Url of web site', String)
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+    if (program.url) {
+        console.log("url " + program.url);
+         var checkJson = checkHtmlUrl(program.url, program.checks);
+    } else if(program.file) {
+        console.log("file " + program.file);
+        var checkJson = checkHtmlFile(program.file, program.checks);
+        var outJson = JSON.stringify(checkJson, null, 4);
+        console.log(outJson);
+    }
+    
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
