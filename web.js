32d400f15be61727fece7848cdc2a137996ce9a0
msyaono <msyaono@rockos.co.jp>
2013-07-17 10:04:13 +0000

    for hw4 part2

--- web.js
+++ web.js
@@ -2,7 +2,7 @@ var express = require('express');
 var fs = require('fs');
 
 var app = express.createServer(express.logger());
-
+app.use(express.static('/home/ubuntu/start_eng/bitstarter'));
 app.get('/', function(request, response) {
     var data = fs.readFileSync('index.html', 'utf8');
 
